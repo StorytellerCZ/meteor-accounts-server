b2396a9f31fedb57f83f777675d4b8783bdd5905
StorytellerCZ <jxd3455@rit.edu>
2016-03-14 17:06:59 -0400

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'storyteller:accounts-server',
-  version: '0.1.3',
+  version: '0.1.4',
   // Brief, one-line summary of the package.
   summary: 'Server side package for storyteller:accounts-react-materialize for username and e-mail operations.',
   // URL to the Git repository containing the source code for this package.
