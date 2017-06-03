1ec98d6fb7789e51bce497afc6e88139441c14cf
StorytellerCZ <storyteller@freedombase.net>
2017-06-03 11:00:40 +0200

    Update depedencies
    Update alanning:roles and prep for next minor release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'storyteller:accounts-server',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'Basic server side for account creation with basic user role and updates to e-mails and username.',
   git: 'https://github.com/StorytellerCZ/meteor-accounts-server',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use(['meteor', 'ecmascript', 'accounts-password', 'check']);
-  api.use(['aldeed:simple-schema@1.5.3', 'matb33:collection-hooks@0.8.4', 'alanning:roles@1.2.15'])
+  api.use(['aldeed:simple-schema@1.5.3', 'matb33:collection-hooks@0.8.4', 'alanning:roles@1.2.16'])
   api.imply(['accounts-password', 'check', 'aldeed:simple-schema', 'matb33:collection-hooks', 'alanning:roles'])
   api.mainModule('accounts-server.js', 'server');
 });
