72e41829cc99cda41ce76ba50e5372f78b7fb092
StorytellerCZ <storyteller@freedombase.net>
2018-01-25 12:59:30 +0100

    Alpha 3

--- accounts-server.js
+++ accounts-server.js
@@ -9,30 +9,58 @@ import { check } from 'meteor/check';
  *
  */
 export function accountsConfig() {
-  Accounts.validateNewUser((user) => {
-    new SimpleSchema({
-      _id: { type: String, regEx: SimpleSchema.RegEx.Id },
-      username: { type: String, index: true, unique: true },
-      emails: { type: Array },
-      'emails.$': { type: Object },
-      'emails.$.address': { type: String, regEx: SimpleSchema.RegEx.Email },
-      'emails.$.verified': { type: Boolean },
-      profile: { type: Object, blackbox: true },
-      createdAt: {
-        type: Date,
-        autoValue() {
-          if (this.isInsert || !this.isFromTrustedCode) {
-            return new Date();
-          }
-        },
-        denyUpdate: true,
-      },
-      services: { type: Object, blackbox: true },
-    }).validate(user);
-
-    // Return true to allow user creation to proceed
-    return true;
+  const userSchema = new SimpleSchema({
+    username: {
+      type: String,
+      // For accounts-password, either emails or username is required, but not both. It is OK to make this
+      // optional here because the accounts-password package does its own validation.
+      optional: true
+    },
+    emails: {
+      type: Array,
+      // For accounts-password, either emails or username is required, but not both. It is OK to make this
+      // optional here because the accounts-password package does its own validation.
+      optional: true
+    },
+    "emails.$": {
+      type: Object
+    },
+    "emails.$.address": {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    },
+    "emails.$.verified": {
+      type: Boolean
+    },
+    createdAt: {
+      type: Date
+    },
+    profile: {
+      type: Object,
+      optional: true,
+      blackbox: true
+    },
+    services: {
+      type: Object,
+      optional: true,
+      blackbox: true
+    },
+    roles: {
+      type: Object,
+      optional: true,
+      blackbox: true
+    },
+    'roles.$': {
+      type: String
+    },
+    // In order to avoid an 'Exception in setInterval callback' from Meteor
+    heartbeat: {
+      type: Date,
+      optional: true
+    }
   });
+
+  Meteor.users.attachSchema(userSchema);
 
   /**
    * Assign basic role
--- package.js
+++ package.js
@@ -1,17 +1,30 @@
 Package.describe({
   name: 'storyteller:accounts-server',
-  version: '1.0.0-alpha.1',
+  version: '1.0.0-alpha.3',
   summary: 'Basic server side for account creation with basic user role and updates to e-mails and username.',
   git: 'https://github.com/StorytellerCZ/meteor-accounts-server',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.1');
+  api.versionsFrom('1.5');
   api.use(['meteor', 'ecmascript', 'accounts-password', 'check']);
-  Npm.depends({'simpl-schema': '1.1.0'});
-  api.use(['matb33:collection-hooks@0.8.4', 'alanning:roles@1.2.16'])
-  api.imply(['accounts-password', 'check', 'matb33:collection-hooks', 'alanning:roles'])
+  api.use([
+    'matb33:collection-hooks@0.8.4',
+    'alanning:roles@1.2.16',
+    'aldeed:collection2-core@2.1.0',
+    'aldeed:schema-index@2.1.1',
+    'aldeed:schema-deny@2.0.1'
+  ]);
+  api.imply([
+    'accounts-password',
+    'check',
+    'matb33:collection-hooks',
+    'alanning:roles',
+    'aldeed:collection2-core',
+    'aldeed:schema-index',
+    'aldeed:schema-deny'
+  ]);
   api.mainModule('accounts-server.js', 'server');
 });
 
