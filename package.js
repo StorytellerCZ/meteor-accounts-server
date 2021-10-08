c5f7d229375b134eec644ca3676297db1f2c5a97
Jan Dvorak <storyteller@freedombase.net>
2021-10-08 15:01:49 +0200

    Deprecate the package

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
   name: 'storyteller:accounts-server',
-  version: '1.0.0-alpha.3',
+  version: '1.0.0',
   summary: 'Basic server side for account creation with basic user role and updates to e-mails and username.',
   git: 'https://github.com/StorytellerCZ/meteor-accounts-server',
-  documentation: 'README.md'
+  documentation: 'README.md',
+  deprecated: true
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.5');
+  api.versionsFrom(['1.5', '2.3']);
   api.use(['meteor', 'ecmascript', 'accounts-password', 'check']);
   api.use([
-    'matb33:collection-hooks@0.8.4',
+    'matb33:collection-hooks@1.1.0',
     'alanning:roles@1.2.16',
     'aldeed:collection2@3.0.0',
     'aldeed:schema-index@3.0.0',
